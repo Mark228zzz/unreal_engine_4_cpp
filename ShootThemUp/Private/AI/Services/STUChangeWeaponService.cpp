461a2d15daa1af6fcdb78908959588f32f4b78db
Mark Mazur <mark.mazur007@gmail.com>
2023-02-18 22:45:45 +0300

    added new service change weapon

---
+++ ShootThemUp/Private/AI/Services/STUChangeWeaponService.cpp
@@ -0,0 +1,27 @@
+// Stoot Them Up Game, All Rights Reserved.
+
+
+#include "AI/Services/STUChangeWeaponService.h"
+#include "Components/STUWeaponComponent.h"
+#include "AIController.h"
+#include "STUUtils.h"
+
+USTUChangeWeaponService::USTUChangeWeaponService()
+{
+	NodeName = "Change Weapon";
+}
+
+void USTUChangeWeaponService::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	const auto Controller = OwnerComp.GetAIOwner();
+	if(Controller)
+	{
+		const auto WeaponComponent = STUUtils::GetSTUPlayerComponent<USTUWeaponComponent>(Controller->GetPawn());
+		if(WeaponComponent && Probability > 0 && FMath::FRand() <= Probability)
+		{
+			WeaponComponent->NextWeapon();
+		}
+	}
+
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+}
